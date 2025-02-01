4b180a7fc8eae6d80be43fab2f11a06d3cefc717
Shawn Toubeau <shawntoubeau@gmail.com>
2025-02-01 17:44:58 -0500

    change DB ID type

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -63,6 +63,7 @@ export default buildConfig({
     pool: {
       connectionString: process.env.DATABASE_URI || '',
     },
+    idType: 'uuid',
   }),
   collections: [Pages, Posts, Media, Categories, Users],
   cors: [getServerSideURL()].filter(Boolean),
