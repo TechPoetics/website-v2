563288f39d8005bf6d67c1aacc665a5accddcb79
Shawn Toubeau <shawntoubeau@gmail.com>
2025-02-02 17:22:26 -0500

    add migration for media title

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250201_231840 from './20250201_231840';
+import * as migration_20250202_222156 from './20250202_222156';
 
 export const migrations = [
   {
     up: migration_20250201_231840.up,
     down: migration_20250201_231840.down,
-    name: '20250201_231840'
+    name: '20250201_231840',
+  },
+  {
+    up: migration_20250202_222156.up,
+    down: migration_20250202_222156.down,
+    name: '20250202_222156'
   },
 ];
